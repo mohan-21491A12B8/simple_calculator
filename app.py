53e4eca4456d94932ab39da433498a2683375752
mohansai <bakkamanthulamohansai@gmail.com>
2024-12-11 21:38:11 +0530

    version-2

--- app.py
+++ app.py
@@ -1,45 +1,67 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from pymongo import MongoClient
+
 app = Flask(__name__)
 my_client = MongoClient("localhost", 27017)
 my_db = my_client["calci"] # database
 results = my_db["results"] # collection
 
+isLoggedIn = False
+credentials = {"steeve@gmail.com":"Steeve#123"}
+
 @app.route("/", methods=["GET", "POST"])
-def homePage():
+def homepage():
+    global isLoggedIn
     if request.method == "POST":
-        n1 = int(request.form["num1"])
-        operation = request.form["operation"]
-        n2 = int(request.form["num2"])
-        if operation == "add":
-            res = f"{n1} + {n2} is {n1+n2}"
-            results.insert_one({
-                "number1":n1, "number2":n2, "operator":operation, "output":res
-            })
-            return render_template("index.html", output=res)
-        elif operation == "sub":
-            res = f"{n1} - {n2} is {n1-n2}"
-            results.insert_one({
-                "number1":n1, "number2":n2, "operator":operation, "output":res
-            })
-            return render_template("index.html", output=res)
-        elif operation == "mul":
-            res = f"{n1} x {n2} is {n1*n2}"
-            results.insert_one({
-                "number1":n1, "number2":n2, "operator":operation, "output":res
-            })
-            return render_template("index.html", output=res)
-        elif operation == "div":
-            try:
-                res = f"{n1} / {n2} is {n1/n2}"
+
+        log_email = request.form["email"]
+        log_password = request.form["password"]
+
+        if (log_email in credentials) and (credentials[log_email]==log_password):
+            isLoggedIn = True
+            return redirect("/calci")
+        else:
+            return redirect("/")
+    else:
+        return render_template("login.html")
+    
+@app.route("/calci", methods=["GET", "POST"])
+def calculator():
+    if isLoggedIn == True:
+        if request.method == "POST":
+            n1 = int(request.form["num1"])
+            opr = request.form["opr"]
+            n2 = int(request.form["num2"])
+            if opr == "add":
+                res = f"{n1} + {n2} is {n1+n2}"
                 results.insert_one({
-                "number1":n1, "number2":n2, "operator":operation, "output":res
+                    "number1":n1, "number2":n2, "operator":opr, "output":res
                  })
                 return render_template("index.html", output=res)
-            except Exception as e:
-                error = "Please change num2 as non-zero"
-                return render_template("index.html", output=error)
-        
-    else:
-        return render_template("index.html")
+            elif opr == "sub":
+                res = f"{n1} - {n2} is {n1-n2}"
+                results.insert_one({
+                    "number1":n1, "number2":n2, "operator":opr, "output":res
+                })
+                return render_template("index.html", output=res)
+            elif opr == "mul":
+                res = f"{n1} x {n2} is {n1*n2}"
+                results.insert_one({
+                    "number1":n1, "number2":n2, "operator":opr, "output":res
+                })
+                return render_template("index.html", output=res)
+            elif opr == "div":
+                try:
+                    res = f"{n1} / {n2} is {n1/n2}"
+                    results.insert_one({
+                    "number1":n1, "number2":n2, "operator":opr, "output":res
+                    })
+                    return render_template("index.html", output=res)
+                except Exception as e:
+                    error = "Please change num2 as non-zero"
+                    return render_template("index.html", output=error)
+
+        else:
+            return render_template("index.html")
+        return redirect("/")
 app.run(debug=True)
